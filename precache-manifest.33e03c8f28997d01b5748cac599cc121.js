fb720f3b901597110ab27747dc0f909b729ed84c
John Doe <johndoe@example.com>
2019-09-21 14:52:17 -0500

    Updates

---
+++ precache-manifest.33e03c8f28997d01b5748cac599cc121.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859aaaf90f67f03de458943be1b0f964",
+    "url": "/riceapps_practice/index.html"
+  },
+  {
+    "revision": "2a2976ed61fb91c73b69",
+    "url": "/riceapps_practice/static/css/main.1afb92d4.chunk.css"
+  },
+  {
+    "revision": "368888f3ba2278b7bb6c",
+    "url": "/riceapps_practice/static/js/2.7eaf85d3.chunk.js"
+  },
+  {
+    "revision": "2a2976ed61fb91c73b69",
+    "url": "/riceapps_practice/static/js/main.250620fe.chunk.js"
+  },
+  {
+    "revision": "0e439833cac39aa67eb9",
+    "url": "/riceapps_practice/static/js/runtime-main.6507dcf9.js"
+  },
+  {
+    "revision": "5feea43e98362b37cd5754d8445067ea",
+    "url": "/riceapps_practice/static/media/personal_pic.5feea43e.jpg"
+  },
+  {
+    "revision": "eb7816a5ce2201da4d977e7d0e8b6a44",
+    "url": "/riceapps_practice/static/media/wiess_logo.eb7816a5.png"
+  }
+]);
