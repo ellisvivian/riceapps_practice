53a5ac00dfe2b2e21438a59ef267955d38f1d8df
John Doe <johndoe@example.com>
2019-09-21 14:43:02 -0500

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from './wiess_logo.png';
+import pic from './personal_pic.jpg';
 import './App.css';
 
 function App() {
+  
+  const [text, setText] = useState("")
+  const [value, setValue] = useState(1)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="team wiess"/>
+        <h1>
+          Vivian Ellis
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cupertino, CA → Houston, TX<br></br>
+          <a
+            className = "tino-link"
+            href = "https://chs.fuhsd.org/"
+            target = "_blank"
+            rel = "noopener noreferrer"
+          >
+            Cupertino High School
+          </a>
+          <br></br>
+          <a
+            className = "rice-link"
+            href = "https://rice.edu"
+            target = "_blank"
+            rel = "noopener noreferrer"
+          >
+            Rice University<br></br>
+          </a>
+          <button type = "button" onClick = {() => {setText("wow you clicked the button")}}>Click Me</button>
+          <br></br>{text}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={pic} className = "pic" alt = "me"/>
       </header>
     </div>
   );
